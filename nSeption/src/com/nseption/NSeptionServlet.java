e71b35353aa85860cb299de04fbc3a3eddb9d110
Steven Mask <mnicpt@gmail.com>
2013-04-02 07:49:03 -0500

    init

---
+++ nSeption/src/com/nseption/NSeptionServlet.java
@@ -0,0 +1,13 @@
+package com.nseption;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class NSeptionServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
