3634991b321e641533e1a18311ab9fe19795f386
Steve Mask <mnicpt@gmail.com>
2014-11-09 21:53:16 -0600

    Added answer for compression

--- public/nPractice/js/codekata.js
+++ public/nPractice/js/codekata.js
@@ -240,7 +240,8 @@ define(function() {
             
 			$correctAnswer.removeClass();
 			$correctAnswer.show();
-			if (solution == kata.sections[section][parseInt(questionId, 10)].output) {
+            alert(solution +" "+kata.sections[section][parseInt(questionId, 10)].output);
+			if (solution === kata.sections[section][parseInt(questionId, 10)].output) {
 				$correctAnswer.addClass("correct");
 				$correctAnswer.text("Correct!");
 			} else {
